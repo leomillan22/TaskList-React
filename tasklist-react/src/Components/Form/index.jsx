eb6026e2481de8b0bc3566e664e64e33cfdf9a4a
leomillan22 <leonardo.amillan@outlook.com>
2024-06-20 11:52:37 -0300

    add components

--- tasklist-react/src/Components/Form/index.jsx
+++ tasklist-react/src/Components/Form/index.jsx
@@ -1,7 +1,16 @@
+import Button from '../Button'
+import Input from '../Input'
 import './form.css'
 
 const Form = () => {
-
+    return (
+        <section>
+            <form >
+                <Input />
+                <Button />
+            </form>
+        </section>
+    )
 }
 
 export default Form
---
+++ tasklist-react/src/Components/Input/index.jsx
@@ -0,0 +1,11 @@
+import './input.css'
+
+const Input = () => {
+    return (
+        <div className='input'>
+            <input placeholder="Add a new task..." />
+        </div>
+    )
+}   
+
+export default Input 
