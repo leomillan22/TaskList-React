99e56b65449d09f00934847c30a01cad83b04ec8
leomillan22 <leonardo.amillan@outlook.com>
2024-06-24 10:08:23 -0300

    update - Tasks component

--- tasklist-react/src/App.jsx
+++ tasklist-react/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Form from './Components/Form'
+import Tasks from './Components/Tasks'
 
 function App() {
  
   return (
     <>
       <Form />
+      <h1>Tasks to do - </h1>
+      <Tasks />
+      <Tasks />
+      <Tasks />
     </>
   )
 }
---
+++ tasklist-react/src/Components/Tasks/index.jsx
@@ -0,0 +1,15 @@
+import './Tasks.css'
+
+const Tasks = () => {
+    return(
+        <section className='Tasks'>
+            <h1>props.nome</h1>
+            <div>
+                <button>C</button>
+                <button>D</button>
+            </div>
+        </section>
+    )
+}
+
+export default Tasks
